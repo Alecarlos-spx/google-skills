92e03e9bb9f6b473e0b6e4dff59eda88339ff167
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-12-22 20:42:44 -0300

    feat(graphs): adiciona implementação

--- estrutura_de_dados/graphs/graph.go
+++ estrutura_de_dados/graphs/graph.go
@@ -5,84 +5,82 @@ import (
 	"fmt"
 )
 
-type Vertex struct {
-	data        interface{}
-	inputEdges  *list.List
-	outputEdges *list.List
+type Vertex struct { // struct representando um vértice
+	data        interface{} // dado que iremos adidionar no vértice
+	inputEdges  *list.List  // arestas de entrada
+	outputEdges *list.List  // arestas de saída
 }
 
-func (v *Vertex) create(value interface{}) {
-	v.data = value
-	v.inputEdges = list.New()
-	v.outputEdges = list.New()
+func (v *Vertex) create(value interface{}) { // função que cria um novo vértice
+	v.data = value             // setando o dado do vértice
+	v.inputEdges = list.New()  // inicia como uma lista vazia
+	v.outputEdges = list.New() // inicia como uma lista vazia
 }
 
-func (v *Vertex) addInputEdge(e *Edge) {
+func (v *Vertex) addInputEdge(e *Edge) { // função que adidiona uma aresta de entrada ao grafo
 	v.inputEdges.PushBack(e)
 }
 
-func (v *Vertex) addOutputEdge(e *Edge) {
+func (v *Vertex) addOutputEdge(e *Edge) { // função que adiciona uma aresta de saída ao grafo
 	v.outputEdges.PushBack(e)
 }
 
-type Edge struct {
-	weight float64
-	start  *Vertex
-	end    *Vertex
+type Edge struct { // struct que representa uma aresta
+	weight float64 // peso da aresta
+	start  *Vertex // vétice de inicio
+	end    *Vertex // vértice de final
 }
 
-func (e *Edge) create(w float64, start, end *Vertex) {
+func (e *Edge) create(w float64, start, end *Vertex) { // função que cria um novo vértice
 	e.weight = w
 	e.start = start
 	e.end = end
 }
 
-type Graph struct {
-	vertex *list.List
-	edges  *list.List
+type Graph struct { // struct que representa um grafo
+	vertex *list.List // vértices do grafo
+	edges  *list.List // arestas do grafo
 }
 
-func (g *Graph) create() {
-	g.vertex = list.New()
-	g.edges = list.New()
+func (g *Graph) create() { // função que cria um grafo
+	g.vertex = list.New() // começa como uma lista vazia
+	g.edges = list.New()  // começa como uma lista vazia
 }
 
-func (g *Graph) addVertex(data interface{}) {
-	var vertex *Vertex = &Vertex{}
-	vertex.create(data)
-
-	g.vertex.PushBack(vertex)
+func (g *Graph) addVertex(data interface{}) { // função que adiciona um vértice ao grafo
+	var vertex *Vertex = &Vertex{} // define um grafo
+	vertex.create(data)            // cria um grafo passando o dado
+	g.vertex.PushBack(vertex)      // adiciona o vértice na lista de vértices do grafo
 }
 
-func (g *Graph) addEdge(weight float64, dataStart, dataEnd interface{}) {
-	var start *Vertex = g.getVertex(dataStart)
-	var end *Vertex = g.getVertex(dataEnd)
+func (g *Graph) addEdge(weight float64, dataStart, dataEnd interface{}) { // função que adiciona uma aresta entre os grafos
+	var start *Vertex = g.getVertex(dataStart) // Pesquisa o vértice através do dado - inicio
+	var end *Vertex = g.getVertex(dataEnd)     // Pesquisa o vértice através do dado - final
 
-	var edge *Edge = &Edge{}
-	edge.create(weight, start, end)
+	var edge *Edge = &Edge{}        // define uma nova aresta
+	edge.create(weight, start, end) // cria uma nova aresta, passando os vértices de início e final
 
-	start.addOutputEdge(edge)
-	end.addInputEdge(edge)
-
-	g.edges.PushBack(edge)
+	start.addOutputEdge(edge) // adiciona a aresta de saida no vértice de ínicio
+	end.addInputEdge(edge)    // adiciona a aresta de entrada no vértice de saída
+	g.edges.PushBack(edge)    // adiciona a aresta na lista de arestas do grafo
 }
 
-func (g *Graph) getVertex(data interface{}) *Vertex {
-	var vertex *Vertex = nil
+func (g *Graph) getVertex(data interface{}) *Vertex { // função que pesquisa um vértice
+	var vertex *Vertex = nil // vértice
 
-	for l := g.vertex.Front(); l != nil; l = l.Next() {
-		v := l.Value.(*Vertex)
+	for l := g.vertex.Front(); l != nil; l = l.Next() { // loop que percorre cada vértice do grafo
+		v := l.Value.(*Vertex) // passando do tipo interface{} para *Vertex
 
-		if v.data == data {
-			vertex = l.Value.(*Vertex)
-			break
+		if v.data == data { // verifica se o dado do vértice atual é igual ao procurado
+			vertex = v // vertex é igual ao v
+			break      // para o loop
 		}
 	}
 
-	return vertex
+	return vertex // retorna o vértice caso seja encontrado ou nil
 }
 
-func (g *Graph) BFS() {
+func (g *Graph) BFS() { // Algoritmo Breadth-first search
 	visiteds := list.New()
 	queue := list.New()
 
@@ -126,21 +124,21 @@ func isVisited(l *list.List, e *Vertex) bool {
 }
 
 func main() {
-	var graph *Graph = &Graph{}
-	graph.create()
+	var graph *Graph = &Graph{} // define um grafo
+	graph.create()              // cria o grafo com os valores iniciais
 
-	graph.addVertex("Guilherme")
-	graph.addVertex("Yasmim")
-	graph.addVertex("João")
-	graph.addVertex("Clebin")
-	graph.addVertex("Rogerin")
+	graph.addVertex("Guilherme") // adiciona um novo vértice com o valor Guilherme
+	graph.addVertex("Yasmim")    // adiciona um novo vértice com o valor Yasmim
+	graph.addVertex("João")      // adiciona um novo vértice com o valor João
+	graph.addVertex("Clebin")    // adiciona um novo vértice com o valor Clebin
+	graph.addVertex("Rogerin")   // adiciona um novo vértice com o valor Rogerin
 
-	graph.addEdge(2, "Guilherme", "Yasmim")
-	graph.addEdge(3, "Yasmim", "Clebin")
-	graph.addEdge(1, "Clebin", "João")
-	graph.addEdge(1, "Guilherme", "João")
-	graph.addEdge(2, "Rogerin", "Yasmim")
-	graph.addEdge(3, "Rogerin", "Guilherme")
+	graph.addEdge(2, "Guilherme", "Yasmim")  // cria uma aresta entre Guilherme e Yasmim
+	graph.addEdge(3, "Yasmim", "Clebin")     // cria uma aresta entre Yasmim e Clebin
+	graph.addEdge(1, "Clebin", "João")       // cria uma aresta entre Clebin e João
+	graph.addEdge(1, "Guilherme", "João")    // cria uma aresta entre Guilherme e João
+	graph.addEdge(2, "Rogerin", "Yasmim")    // cria uma aresta entre Rogerin e Yasmim
+	graph.addEdge(3, "Rogerin", "Guilherme") // cria uma aresta entre Rogerin e Guilherme
 
 	graph.BFS()
 }
